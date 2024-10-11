7d6cd73242bcf8d928804d24b98e0bdf7b027c8a
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-10-10 22:33:02 -0400

    Refactor header component

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export enum SiteInfo {
+  TITLE = "My Site",
+  DESCRIPTION = "My site description",
+  KEYWORDS = "my, site, keywords",
+  AUTHOR = "My Name",
+  EMAIL = "test@gmail.com",
+  PHONE = "123-456-7890",
+  ADDRESS = "1234 Elm St, Springfield, IL 62701",
+}
+
+export const NavLinks = [
+  { name: "Home", url: "/" },
+  { name: "About", url: "/about" },
+  { name: "Projects", url: "/projects" },
+  { name: "Blog", url: "/blog" },
+  { name: "Contact", url: "/contact" },
+];
